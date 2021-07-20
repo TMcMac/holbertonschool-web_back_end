d627bf4dee4f302fe64a0914f19c8be0ad032e76
TMcMac <timmcmackenjr@gmail.com>
2021-07-20 02:02:10 +0000

    Fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Working with annotations"""
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Method: to_kv - make a tuple out of str and int or float squared
     Parameters: k is a string, v is int or float
