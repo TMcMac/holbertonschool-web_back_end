39049dbbb4121f7b2242ffe44654e73990038326
TMcMac <timmcmackenjr@gmail.com>
2021-07-22 02:14:48 +0000

    pycodestylefixes

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,14 +8,12 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """
     Method: measure_runtime - execute async_comprehension
-            four times in parallel 
+            four times in parallel
     Parameters: none
     Return: returns runtime from measure_runtime
     """
     start = time.time()
-    await asyncio.gather(
-        *(async_comprehension() for i in range(4))
-    )
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
     end = time.time()
 
     return end - start
