781903eba44398634186783e20c89b03ca73df3a
TMcMac <timmcmackenjr@gmail.com>
2021-07-26 02:21:46 +0000

    Adds 1

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Create a class FIFOCache that inherits from BaseCaching
+and is a caching system:
+
+* You must use self.cache_data - dictionary from the parent
+  class BaseCaching
+    - You can overload def __init__(self): but don’t forget
+      to call the parent init: super().__init__()
+
+* def put(self, key, item):
+    - Must assign to the dictionary self.cache_data the item value
+      for the key key.
+    - If key or item is None, this method should not do anything.
+    - If the number of items in self.cache_data is higher that
+      BaseCaching.MAX_ITEMS:
+          + you must discard the first item put in cache(FIFO algorithm)
+          + you must print DISCARD: with the key discarded and
+            following by a new line
+
+* def get(self, key):
+    - Must return the value in self.cache_data linked to key.
+    - If key is None or if the key doesn’t exist in self.cache_data,
+      return None.
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    This class will inherit self.cache_data from BaseCashing
+    but wil not have a limit
+    """
+
+
+    def __init__(self):
+        """
+        Init from BaseCaching
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Adds key value pairs to self.cache_data
+        """
+        if key is not None and item is not None:
+            keyList = list(self.cache_data)[0:]
+            if key in keyList:
+                del self.cache_data[key]
+                keyList.remove(key)
+            self.cache_data[key] = item
+
+            if len(keyList) > BaseCaching.MAX_ITEMS:
+                remove = keyList.pop(0)
+                print("DISCARD: " + key)
+                del self.cache_data[key]
+        else:
+            pass
+
+    def get(self, key):
+        """
+        Retrieves items from self.cache_data by key
+        """
+        try:
+            return self.cache_data[key]
+        except Exception:
+            pass
