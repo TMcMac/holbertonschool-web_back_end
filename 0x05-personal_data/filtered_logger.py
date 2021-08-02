0eba0a99ec19be84bf3b3ddc6e1e75b98aaf89d1
TMcMac <timmcmackenjr@gmail.com>
2021-08-02 14:15:58 +0000

    Adds

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -18,8 +18,31 @@
     to perform the substitution with a single regex.
 """
 import re
+import logging
 from typing import List
 
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        method to filter values in incoming log records using filter_datum.
+        Values for fields in fields should be filtered.
+        """
+        return filter_datum(
+            self.fields, self.REDACTION, super().format(record),
+            self.SEPARATOR)
 
 def filter_datum(fields: List[str],
                  redaction: str,
@@ -27,7 +50,7 @@ def filter_datum(fields: List[str],
                  separator: str) -> str:
     """Takes in a message and obsficates indicated fields"""
     for field in fields:
-        message = re.sub(field + '=' + '.+?' +  separator,
+        message = re.sub(field + '=' + '.+?' + separator,
                          field + '=' + redaction + separator,
                          message)
     return message
