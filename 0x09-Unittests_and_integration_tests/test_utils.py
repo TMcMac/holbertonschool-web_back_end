0fb6da606b5d5779b9ad42bd471c43e5c74ca684
Tim <timmcmackenjr@gmail.com>
2021-08-24 12:45:28 -0500

    Adds exercise 2

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -2,11 +2,11 @@
 """ Tests for utils.py """
 from parameterized import parameterized
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ This class will test all the function
+    """ This class will test the function
         access_nested_map in utils.py
     """
     @parameterized.expand([
@@ -26,6 +26,23 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Test for fail conditions """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ This class will test the function
+        get_json in utils.py
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, test_payload):
+        """ Use mock to test an http call and json return """
+        with unittest.mock.patch('requests.get') as mock:
+            mock().json.return_value = test_payload
+            mock.assert_called_once()
+            result = get_json(url)
+            mock.assertEqual(result, test_payload)
 
 if __name__ == '__main__':
     unittest.main()
