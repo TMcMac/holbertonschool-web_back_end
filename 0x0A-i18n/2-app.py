0574518fe302e8ad22d57488aa40b24636d9deef
Tim <timmcmackenjr@gmail.com>
2021-08-26 13:48:22 -0500

    Fixes config in 1 and adds 2

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -16,6 +16,12 @@ class Config(object):
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    """Get best langauge for user"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def index():
     """Return simple homepage"""
