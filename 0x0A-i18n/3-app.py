dba1dc8e4fcd3611ce954d61a88bbdcbac3b72ba
Tim <timmcmackenjr@gmail.com>
2021-08-26 14:41:12 -0500

    Adds 3

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Simple flask app setup"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
@@ -25,7 +25,7 @@ def get_locale():
 @app.route('/')
 def index():
     """Return simple homepage"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
