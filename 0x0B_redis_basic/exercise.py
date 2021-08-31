475a6f2197c5047f3e517d15793fcb46452b69d2
Tim <timmcmackenjr@gmail.com>
2021-08-31 16:16:02 -0500

    Adds

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,6 +5,50 @@ from typing import Callable, Optional, Union
 import uuid
 
 
+def count_calls(method: Callable) -> Callable:
+    """Count number of times functions called"""
+    @wraps(method)
+    def count_calls_wrapper(self, *args) -> bytes:
+        """Counts calls wrapped function makes"""
+        self._redis.incr(method.__qualname__)
+        return method(self, *args)
+
+    return count_calls_wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """Store history of inputs/outputs for a function"""
+    inputs = f"{method.__qualname__}:inputs"
+    outputs = f"{method.__qualname__}:outputs"
+
+    @wraps(method)
+    def call_history_wrapper(self, *args) -> bytes:
+        """Stores the history of inputs and outputs for a function"""
+        self._redis.rpush(inputs, str(args))
+        out = method(self, *args)
+        self._redis.rpush(outputs, out)
+
+        return out
+
+    return call_history_wrapper
+
+
+def replay(method: Callable) -> str:
+    """Display the history of calls of a particular function"""
+    name = method.__qualname__
+    inputs = f"{name}:inputs"
+    outputs = f"{name}:outputs"
+    r = redis.Redis()
+    data = r.get(name).decode('utf-8')
+    input_list = r.lrange(inputs, 0, -1)
+    output_list = r.lrange(outputs, 0, -1)
+
+    print(f"{name} was called {data} times:")
+
+    for ins, outs in zip(input_list, output_list):
+        print(f"{name}(*{ins.decode('utf-8')}) -> {outs.decode('utf-8')}")
+
+
 class Cache():
     """Cache class"""
 
