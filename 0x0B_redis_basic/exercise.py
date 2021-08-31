8f48bd92ed539e1ef44258778264567be553978b
TMcMac <timmcmackenjr@gmail.com>
2021-08-31 20:32:41 +0000

    Adds 1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -20,3 +20,22 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """get data based on key, return data as original type using fn"""
+        if fn:
+            value = self._redis.get(key)
+            return fn(value)
+        return self._redis.get(key)
+
+
+    def get_str(self, key: str) -> str:
+        """Cast the key to str"""
+        return self.get(key, str)
+
+
+    def get_int(self, key: str) -> int:
+        """Cast the key to int"""
+        return self.get(key, int)
