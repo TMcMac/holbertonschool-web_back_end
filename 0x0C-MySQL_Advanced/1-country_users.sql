4d5ecb81343d3fac11235dc017a7bfed10ef45e7
TMcMac <timmcmackenjr@gmail.com>
2021-09-02 18:53:57 +0000

    Fixes syntax error

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,7 @@
 -- creates a table users
 -- id(int PK), email(str), name(str), country
-
 CREATE TABLE IF NOT EXISTS users (
        id int NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
        email varchar(255) NOT NULL UNIQUE,
-       name varchar(255)
+       name varchar(255),
        country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL);
