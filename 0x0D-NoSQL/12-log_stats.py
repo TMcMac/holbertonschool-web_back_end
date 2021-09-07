a4568749d789d7ef7446c1b4e4bfe1bde37d1b2a
TMcMac <timmcmackenjr@gmail.com>
2021-09-07 23:47:26 +0000

    Adds

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Practicing pymongo """
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+
+    total = collection.count_documents({})
+    get = collection.count_documents({'method': 'GET'})
+    post = collection.count_documents({'method': 'POST'})
+    put = collection.count_documents({'method': 'PUT'})
+    patch = collection.count_documents({'method': 'PATCH'})
+    delete = collection.count_documents({'method': 'DELETE'})
+    status = collection.count_documents({'method': 'GET', 'path': '/status'})
+    nl = '\n'
+
+    print(f"{total} logs{nl}\
+Methods:{nl}\
+\tmethod GET: {get}{nl}\
+\tmethod POST: {post}{nl}\
+\tmethod PUT: {put}{nl}\
+\tmethod PATCH: {patch}{nl}\
+\tmethod DELETE: {delete}{nl}\
+{status} status check")
+
