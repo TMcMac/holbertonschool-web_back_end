73562da5083aa1afd7316ff873006294cd52cc29
TMcMac <timmcmackenjr@gmail.com>
2021-09-14 19:34:31 +0000

    Adds 1

---
+++ 0x0F-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+//Return a promise from a fake api call
+function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+	if (success) {
+	    resolve({
+		status: 200,
+		body: 'Success'
+	    });
+	} else {
+	    eject(new Error("Not the API you're looking for"));
+	}
+    });	
+}
+export default getFullResponseFromAPI;
