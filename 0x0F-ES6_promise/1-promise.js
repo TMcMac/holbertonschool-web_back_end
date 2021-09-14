bd3bb5c639362949f91588fee0af6b9651c656ec
TMcMac <timmcmackenjr@gmail.com>
2021-09-14 19:37:59 +0000

    Fix for success condition

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -2,10 +2,10 @@
 function getFullResponseFromAPI(success) {
     return new Promise((resolve, reject) => {
 	if (success) {
-	    resolve({
-		status: 200,
-		body: 'Success'
-	    });
+	   resolve({
+               status: 200,
+               body: 'Success',
+	   });
 	} else {
 	    eject(new Error("Not the API you're looking for"));
 	}
