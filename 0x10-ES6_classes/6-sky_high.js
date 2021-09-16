ba8af93e4bfd750f6c2693199a9ebc421c776861
TMcMac <timmcmackenjr@gmail.com>
2021-09-16 15:50:25 +0000

    Adds 5 and 6

---
+++ 0x10-ES6_classes/6-sky_high.js
@@ -0,0 +1,17 @@
+// Extends class building
+import Building from './5-building.js'
+
+class SkyHighBuilding extends Building {
+    consructor(sqft, floors) {
+	super(sqft);
+	this._floors = floors;
+    }
+
+    get sqft() {return this._sqft}
+    get floors() {return this.floors}
+
+    evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
+default export SkyHighBuilding;
