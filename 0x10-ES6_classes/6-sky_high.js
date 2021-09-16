d8c36c6c122cc2c6af30d5c6217d779ee5f4d788
Tim <timmcmackenjr@gmail.com>
2021-09-16 17:48:56 -0500

    Lint fixes

--- 0x10-ES6_classes/6-sky_high.js
+++ 0x10-ES6_classes/6-sky_high.js
@@ -3,12 +3,12 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-	  super(sqft);
-	  this._floors = floors;
+    super(sqft);
+    this._floors = floors;
   }
 
-  get sqft() {return this._sqft}
-  get floors() {return this.floors}
+  get sqft() { return this._sqft }
+  get floors() { return this.floors }
 
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
