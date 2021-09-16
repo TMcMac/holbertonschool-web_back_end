d358c29ae93f92f8c573d8537000e5da24296358
Tim <timmcmackenjr@gmail.com>
2021-09-16 16:17:01 -0500

    Lint fix

--- 0x10-ES6_classes/1-make_classrooms.js
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -3,6 +3,6 @@ import ClassRoom from './0-classroom.js';
 
 const initializeRooms = () => {
   const array = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-  return array
+  return array;
 };
 export default initializeRooms;
