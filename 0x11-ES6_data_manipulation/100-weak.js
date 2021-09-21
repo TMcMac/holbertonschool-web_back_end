f03586a01fea6c809ddd17f25d376a33e18dd651
TMcMac <timmcmackenjr@gmail.com>
2021-09-21 23:26:05 +0000

    Adds extra

---
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+// Export a const instance of WeakMap and name it weakMap
+
+const weakMap = new WeakMap();
+const queryAPI = (endpoint) => {
+  if (!weakMap.get(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  }
+
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
+};
+
+export { weakMap, queryAPI };
